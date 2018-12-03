624baf63746b4cf51314c8bd4740a2c54185615a
gura <oli999@naver.com>
2018-12-03 18:12:21 +0900

    남의 파일을 삭제할수 없도록 fix 함

--- Spring07_Final/src/main/java/com/gura/spring07/file/service/FileService.java
+++ Spring07_Final/src/main/java/com/gura/spring07/file/service/FileService.java
@@ -1,8 +1,10 @@
 package com.gura.spring07.file.service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface FileService {
 	public void getList(HttpServletRequest request);
-	public void removeFileInfo(int num, HttpServletRequest request);
+	public void removeFileInfo(int num, HttpServletRequest request, 
+			HttpServletResponse response);
 }
